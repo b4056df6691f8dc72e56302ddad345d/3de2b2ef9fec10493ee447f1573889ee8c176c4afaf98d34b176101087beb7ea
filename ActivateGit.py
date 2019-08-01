cc7f1784d426ba356162f37ab303f975e3e99bdf
b4056df6691f8dc72e56302ddad345d <b4056df6691f8dc72e56302ddad345@gmail.com>
2019-08-01 14:38:19 -0400

    Adding Data Via Python3.7

---
+++ ActivateGit.py
@@ -0,0 +1,17 @@
+import os
+
+print('Stepping In To Directory')
+print('Showing Dump Wallets')
+print(os.listdir())
+print('Initializing Git')
+os.system('git init')
+print('Adding All Files To Git')
+os.system('git add .')
+print('Adding Commit Message')
+os.system('git commit -m "Adding Data Via Python3.7"')
+print('Adding Remote Origin To Database')
+os.system('git remote add origin https://github.com/b4056df6691f8dc72e56302ddad345d/3de2b2ef9fec10493ee447f1573889ee8c176c4afaf98d34b176101087beb7ea.git')
+print('Attempting To Add Files To Repository')
+os.system('git push -u origin master')
+print('Stepping Back To Main Area.')
+
