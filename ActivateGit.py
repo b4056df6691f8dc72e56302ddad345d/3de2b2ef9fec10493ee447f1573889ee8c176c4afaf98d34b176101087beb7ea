92ba9a9f5675aa480d4909260b9b626a1ebf618d
b4056df6691f8dc72e56302ddad345d <b4056df6691f8dc72e56302ddad345@gmail.com>
2019-08-01 15:41:12 -0400

    Adding Data Via Python3.7

--- ActivateGit.py
+++ ActivateGit.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 
-print('Cloning Git Repo...')
-os.system('git clone https://github.com/b4056df6691f8dc72e56302ddad345d/3de2b2ef9fec10493ee447f1573889ee8c176c4afaf98d34b176101087beb7ea.git')
-print('Adding All Files To Git')
-for i in os.listdir('C:/Users/Administrator/Desktop/Bitcoin/Wallets/Dump/3de2b2ef9fec10493ee447f1573889ee8c176c4afaf98d34b176101087beb7ea'):
- if '.vnm' in i:
-  shutil.move('C:/Users/Administrator/Desktop/Bitcoin/Wallets/Dump/3de2b2ef9fec10493ee447f1573889ee8c176c4afaf98d34b176101087beb7ea/{}'.format(i), 'C:/Users/Administrator/Desktop/Bitcoin/Wallets/Dump/{}'.format(i))
-print('Removing Cloned Directory')
+#print('Cloning Git Repo...')
+#os.system('git clone https://github.com/b4056df6691f8dc72e56302ddad345d/3de2b2ef9fec10493ee447f1573889ee8c176c4afaf98d34b176101087beb7ea.git')
+#print('Adding All Files To Git')
+#for i in os.listdir('C:/Users/Administrator/Desktop/Bitcoin/Wallets/Dump/3de2b2ef9fec10493ee447f1573889ee8c176c4afaf98d34b176101087beb7ea'):
+# if '.vnm' in i:
+#  shutil.move('C:/Users/Administrator/Desktop/Bitcoin/Wallets/Dump/3de2b2ef9fec10493ee447f1573889ee8c176c4afaf98d34b176101087beb7ea/{}'.format(i), 'C:/Users/Administrator/Desktop/Bitcoin/Wallets/Dump/{}'.format(i))
+#print('Removing Cloned Directory')
 os.system('git add .')
 print('Adding Commit Message')
 os.system('git commit -m "Adding Data Via Python3.7"')
